e09ba50576b1756b630819fc1fa48ea745498003
Kalapala Rineesh <sunnyrineesh07@gmail.com>
2026-09-10 15:44:35 +0530

    Time: 7 ms (70.45%), Space: 19.2 MB (56.16%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,18 +1,9 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        alpha ='abcdefghijklmnopqrstuvwxyz'
-        values = []
-        d = {}
-        i = 26
-        while i >= 0 :
-            values.append(i)
-            i -= 1
-        for i in range(26):
-            d[alpha[i]] = values[i]
-        print(d)
         ans = 0
-        for i in range(0,len(s)):
-            for k , v  in d.items():
-                if s[i] == k:
-                    ans += v * (i + 1 )
+        
+        for i in range(len(s)):
+            value =  26 - (ord(s[i]) - ord('a'))
+            ans += value * (i + 1)
+        
         return ans
